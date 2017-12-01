4bfaa15aa34410c36ab01804b8b4d9436f7a2f8d
esteban1234 <tva_libra@hotmail.com>
2017-12-01 14:29:09 -0600

    metas

--- vistas/encabezado2.php
+++ vistas/encabezado2.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
-	
+	<meta name="google-site-verification" content="8xCFUUDoZUrhqGegz_8cHi4fZoT_mZIEfwUu1WEUmTo" />
 	<link rel="icon" type="image/png" href="../img/favicon.png" />
 	<title>GIOTEX INMOBILIARIA</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
